d7fe9ff0ca16e5ba161cc5f6ca0abfad732c5a39
unknown <joshua537.nit@gmail.com>
2021-12-09 16:03:22 +0530

    added create_df.py

--- create_df.py
+++ create_df.py
@@ -1,9 +1,21 @@
+from pyspark.sql import Row
+from pyspark.sql.functions import lit
+from pyspark.sql.types import StructField, StructType, StringType, LongType, IntegerType
 import init_context
 
 sc,sp = init_context.init_context()
-
-def add_col(col_name,data_type):
-    df = sp.createDataFrame()
+def create_schema():
+    myManualSchema = StructType([
+        StructField("sno", IntegerType(), True),
+    ])
+    df = sp.createDataFrame([],myManualSchema)
+    return df
+def add_col(df,col_name,data_type):
+    if df:
+        df.withColumn(col_name,lit(None))
+    else:
+        df = create_schema()
+        df.withColumn(col_name,lit())
 
 def drop_col(col_name):
     pass
